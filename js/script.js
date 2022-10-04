a8cb72b93daf39f75e83e46b87c35e34fda07fd2
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-10-04 01:09:48 -0300

    script.js
    circle and img parallax

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ $(document).ready(function () {
 
   let circleA = new ProgressBar.Circle(containerA, {
     color: '#64DAF9',
-    stroke: 8,
+    strokeWidth: 8,
     duration: 1400,
     from: { color: '#AAA' },
-    to: { color: '#650AF9' },
+    to: { color: '#65daf9' },
 
     step: function (state, circle) {
       circle.path.setAttribute('stroke', state.color)
@@ -18,5 +18,80 @@ $(document).ready(function () {
       circle.setText(value)
     },
   })
-  circleA.animate(1.0)
+
+  let containerB = document.getElementById('circleB')
+
+  let circleB = new ProgressBar.Circle(containerB, {
+    color: '#64DAF9',
+    strokeWidth: 8,
+    duration: 1600,
+    from: { color: '#AAA' },
+    to: { color: '#65daf9' },
+
+    step: function (state, circle) {
+      circle.path.setAttribute('stroke', state.color)
+
+      let value = Math.round(circle.value() * 254)
+
+      circle.setText(value)
+    },
+  })
+
+  let containerC = document.getElementById('circleC')
+
+  let circleC = new ProgressBar.Circle(containerC, {
+    color: '#64DAF9',
+    strokeWidth: 8,
+    duration: 1900,
+    from: { color: '#AAA' },
+    to: { color: '#65daf9' },
+
+    step: function (state, circle) {
+      circle.path.setAttribute('stroke', state.color)
+
+      let value = Math.round(circle.value() * 30)
+
+      circle.setText(value)
+    },
+  })
+
+  let containerD = document.getElementById('circleD')
+
+  let circleD = new ProgressBar.Circle(containerD, {
+    color: '#64DAF9',
+    strokeWidth: 8,
+    duration: 2100,
+    from: { color: '#AAA' },
+    to: { color: '#65daf9' },
+
+    step: function (state, circle) {
+      circle.path.setAttribute('stroke', state.color)
+
+      let value = Math.round(circle.value() * 600)
+
+      circle.setText(value)
+    },
+  })
+  //iniciando o loader quando o usuario chega no elemento
+  let dataAreaOffset = $('#data-area').offset()
+  let stop = 0
+
+  $(window).scroll(function (e) {
+    let scroll = $(window).scrollTop()
+    if (scroll > dataAreaOffset.top - 500 && stop == 0) {
+      circleA.animate(1.0)
+      circleB.animate(1.0)
+      circleC.animate(1.0)
+      circleD.animate(1.0)
+
+      stop = 1
+    }
+  })
+  //Parallax 
+setTimeout(function(){
+
+$('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'});
+
+}, 250)
+
 })
