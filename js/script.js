2eb2ec21c42326c251753a181758219c1e324866
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-10-05 22:27:57 -0300

    script.js
    implementação 22:28

--- js/script.js
+++ js/script.js
@@ -91,7 +91,7 @@ $(document).ready(function () {
 setTimeout(function(){
 
 $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'});
-
+$('#apply-area').parallax({ imageSrc: 'img/pattern.png' })
 }, 250)
 
 })
