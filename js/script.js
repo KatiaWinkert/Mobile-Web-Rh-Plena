b826590ca0716c1c25e32802260e44ddd0892b16
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-10-06 23:40:32 -0300

    js
    implementação

--- js/script.js
+++ js/script.js
@@ -98,5 +98,31 @@ $(document).ready(function () {
   $('.filter-btn').on('click', function () {
     let type = $(this).attr('id')
     let boxes = $('.project-box')
+
+    $('.main-btn').removeClass('active')
+    $(this).addClass('active')
+
+    if (type == 'adm-btn') {
+      eachBoxes('adm', boxes)
+    } else if (type == 'vendas-btn') {
+      eachBoxes('vendas', boxes)
+    } else if (type == 'lang-btn') {
+      eachBoxes('lang', boxes)
+    } else {
+      eachBoxes('all', boxes)
+    }
   })
+  function eachBoxes(type, boxes) {
+    if (type == 'all') {
+      $(boxes).fadeIn()
+    } else {
+      $(boxes).each(function () {
+        if (!$(this).hasClass(type)) {
+          $(this).fadeOut('slow')
+        } else {
+          $(this).fadeIn()
+        }
+      })
+    }
+  }
 })
